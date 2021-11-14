c85450d654066a4c291925fef3ee195a4bfd8079
Gambsantoine <dede@worldofentaria.eu>
2021-11-14 17:18:02 +0100

    Update mojang.js

--- app/assets/js/mojang.js
+++ app/assets/js/mojang.js
@@ -27,30 +27,6 @@ const statuses = [
         status: 'grey',
         name: 'Authentication Service',
         essential: true
-    },
-    {
-        service: 'textures.minecraft.net',
-        status: 'grey',
-        name: 'Minecraft Skins',
-        essential: false
-    },
-    {
-        service: 'api.mojang.com',
-        status: 'grey',
-        name: 'Public API',
-        essential: false
-    },
-    {
-        service: 'minecraft.net',
-        status: 'grey',
-        name: 'Minecraft.net',
-        essential: false
-    },
-    {
-        service: 'account.mojang.com',
-        status: 'grey',
-        name: 'Mojang Accounts Website',
-        essential: false
     }
 ]
 
